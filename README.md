d4e08829fd3b3d9dbf35ef802db9d70da946a4f7
ChickenBlue <khang2003.world@gmail.com>
2025-02-24 19:25:00 +0700

    Create-Project

---
+++ README.md
@@ -0,0 +1,27 @@
+## Manager System
+
+## Giới thiệu
+Manager System là một hệ thống quản lý dữ liệu được xây dựng bằng Electron.js.
+Manager System giúp người dùng quản lý Databasee mà không cần biết về SQL
+
+## Cài đặt
+Chương trình chưa bao gồm các modules của Electron.js, vì vậy bạn cần cài đặt thủ công bằng lệnh sau:
+```sh
+npm install
+```
+
+## Khởi chạy dự án
+
+Để khởi chạy dự án bạn cần thực hiện câu lệnh:
+
+```sh
+npm run start
+```
+
+## Kết nối với GitHub
+
+Chạy cậu lệnh dưới dây vào terminal để thực hiện kết nối
+
+```sh
+git remote add origin https://github.com/ChickenBluee/Manager-System.git
+```
---
+++ main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require('electron/main')
+app.commandLine.appendSwitch('disable-gpu');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    resizable: false,
+    maximizable: false,
+    titleBarStyle: 'hidden',
+    titleBarOverlay: {
+      color: '#FFFFFF00',
+      symbolColor: '#FFFFFF',
+      height: 29,
+    },
+  
+    webPreferences: {
+      nodeIntegrationInWorker: true,
+      webSecurity: false,
+      allowRunningInsecureContent: true, 
+    }
+  })
+  win.loadFile('src/html/index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
