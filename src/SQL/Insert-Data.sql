9e84eaf750a4985fdc9944b117de7bf3e4a8d13f
ChickenBlue <khang2003.world@gmail.com>
2025-02-24 20:02:23 +0700

    Upload-SQL

--- src/SQL/Insert-Data.sql
+++ src/SQL/Insert-Data.sql
@@ -5,7 +5,6 @@ VALUES (N'admin'), (N'moderator'), (N'user');
 
 INSERT INTO ACCOUNT (_ACCOUNT_, _PASSWORD_, _ROLE_) 
 VALUES 
-    ('nhiet',           '1',        'admin'),
     ('Administrastor',  'abc123!',  'admin'), 
     ('admin',           '123',      'admin'), 
     ('khang',           '1',        'admin'),
